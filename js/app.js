88ccc876dd81de060ff02dca2a8636427691ecae
sargoo <goon_aguea@hotmail.com>
2024-05-23 22:01:23 -0300

    se crean constructos y primer prototype

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//Constructors
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI() {}
+
+
+// llena las opciuones de los años
+
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+        min = max - 24;
+
+    const selectYear = document.querySelector('#year');
+    for (let i = max; i > min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+// instanciar UI
+
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones();
+})
